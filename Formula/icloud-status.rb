cbdef6cd78b17b92cbce06915b90f5fb904a6837
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-02-09 15:55:17 +0100

    Update SHA for v0.1.0

--- Formula/icloud-status.rb
+++ Formula/icloud-status.rb
@@ -2,7 +2,7 @@ class IcloudStatus < Formula
   desc "macOS CLI tool to display comprehensive iCloud status information"
   homepage "https://github.com/aladac/icloud-status"
   url "https://github.com/aladac/icloud-status/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8ccdc5d3ae053ea219865d6dd272eafb1eb61fe290acb7cc64411fae708de033"
+  sha256 "68d356dd8966c44cc2e0cabc9a74e0d680595313b192d4137ee5d487f876d09e"
   license "MIT"
   head "https://github.com/aladac/icloud-status.git", branch: "master"
 
