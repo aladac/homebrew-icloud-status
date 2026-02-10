d120ab59d94a8f36bf1e038e4f5aa0b986ee97d2
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-02-10 21:09:11 +0100

    Update formula URLs to saiden-dev

--- Formula/icloud-status.rb
+++ Formula/icloud-status.rb
@@ -1,10 +1,10 @@
 class IcloudStatus < Formula
   desc "macOS CLI tool to display comprehensive iCloud status information"
-  homepage "https://github.com/aladac/icloud-status"
-  url "https://github.com/aladac/icloud-status/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/saiden-dev/icloud-status"
+  url "https://github.com/saiden-dev/icloud-status/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "68d356dd8966c44cc2e0cabc9a74e0d680595313b192d4137ee5d487f876d09e"
   license "MIT"
-  head "https://github.com/aladac/icloud-status.git", branch: "master"
+  head "https://github.com/saiden-dev/icloud-status.git", branch: "master"
 
   depends_on xcode: ["14.0", :build]
   depends_on :macos
