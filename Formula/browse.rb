72690ffd389bf459672fd2df2cfc6c5d9807bb7d
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-02-16 01:45:41 +0100

    Add browse formula (v0.2.15)

---
+++ Formula/browse.rb
@@ -0,0 +1,23 @@
+class Browse < Formula
+  desc "Headless browser automation for Claude Code using Playwright WebKit"
+  homepage "https://github.com/saiden-dev/browse"
+  url "https://registry.npmjs.org/@saiden/browse/-/browse-0.2.15.tgz"
+  sha256 "07a8c0b87194bab20c8810b177fc77dc10c46cb277ad7342db45350bf1276984"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def post_install
+    # Install Playwright WebKit browser
+    system "npx", "--prefix", libexec, "playwright", "install", "webkit"
+  end
+
+  test do
+    assert_match "browse", shell_output("#{bin}/browse --help")
+  end
+end
