3f73f2d028a456b2774d393caf0f93cb44674e2d
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-01 21:07:14 +0200

    Update tengu-init to 0.3.3 with Linux support

--- Formula/tengu-init.rb
+++ Formula/tengu-init.rb
@@ -1,19 +1,31 @@
 class TenguInit < Formula
   desc "Provision Tengu PaaS on Hetzner Cloud"
   homepage "https://github.com/saiden-dev/tengu-init"
-  version "0.1.5"
+  version "0.3.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/saiden-dev/tengu-init/releases/download/v0.1.5/tengu-init-apple-silicon"
-      sha256 "d5942d6dbc8f22dfdfe511c0588e2c15413241ce94bcee8188b14524c8e908a0"
+      url "https://github.com/saiden-dev/tengu-init/releases/download/v#{version}/tengu-init-apple-silicon"
+      sha256 "dc16ae6a2e88420df0edababe814ab1ba7f158778adfe901e53e626880a576d4"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/saiden-dev/tengu-init/releases/download/v#{version}/tengu-init-linux-arm64"
+      sha256 "6aa81436a2a75533329897ba4e71f17eec7aed0dd13c8fd273d05e4b665c97e4"
+    end
+
+    on_intel do
+      url "https://github.com/saiden-dev/tengu-init/releases/download/v#{version}/tengu-init-linux-amd64"
+      sha256 "bbc5d2d8f7d6d8f2cca85efdfa5c2fec1fabaaf72dfdf6cde345edd25978e5d0"
     end
   end
 
   def install
-    binary_name = "tengu-init-apple-silicon"
-    bin.install binary_name => "tengu-init"
+    binary = Dir["tengu-init-*"].first || "tengu-init"
+    bin.install binary => "tengu-init"
   end
 
   test do
