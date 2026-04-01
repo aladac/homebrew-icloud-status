227dd43753204027166e5d5e4108ea709e4cf73c
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-01 21:07:13 +0200

    Update icloud-status to 0.1.2

--- Formula/icloud-status.rb
+++ Formula/icloud-status.rb
@@ -1,8 +1,8 @@
 class IcloudStatus < Formula
   desc "macOS CLI tool to display comprehensive iCloud status information"
   homepage "https://github.com/saiden-dev/icloud-status"
-  url "https://github.com/saiden-dev/icloud-status/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "68d356dd8966c44cc2e0cabc9a74e0d680595313b192d4137ee5d487f876d09e"
+  url "https://github.com/saiden-dev/icloud-status/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "686e13d64f606c022056011c2db223d333d1ddfdad29b9b091094026a4639c70"
   license "MIT"
   head "https://github.com/saiden-dev/icloud-status.git", branch: "master"
 
