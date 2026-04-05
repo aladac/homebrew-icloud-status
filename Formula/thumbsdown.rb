d09ed5e7affb86905989eafdeb49f7c5159e392d
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-05 17:21:37 +0000

    Update thumbsdown to 0.1.4

--- Formula/thumbsdown.rb
+++ Formula/thumbsdown.rb
@@ -1,25 +1,25 @@
 class Thumbsdown < Formula
   desc "Generate thumbnail grids from video files using ffmpeg"
   homepage "https://github.com/aladac/thumbsdown"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/aladac/thumbsdown/releases/download/v#{version}/thumbsdown-macos-arm64"
-      sha256 "27594c19e95b1c6a3db2c2228bf3270b03e05db016b10ace67d9298bc0b88d43"
+      sha256 "f45b9691b3cf82fdbe858d8db160ddaca1b1af963bd4952afc7ff8a4f550dff3"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/aladac/thumbsdown/releases/download/v#{version}/thumbsdown-linux-arm64"
-      sha256 "a6a8264b9f989ace6566dd3a76a4b57861f25480f0154fd4c1f2d627537a6476"
+      sha256 "7fb224694be50e93906344682d5db79f9904a086df22f40545913fe4e8a4cf36"
     end
 
     on_intel do
       url "https://github.com/aladac/thumbsdown/releases/download/v#{version}/thumbsdown-linux-amd64"
-      sha256 "60ad6a8ad5d1af7e98782301214f34473c5395745dd7e2b0ec95615ee6bfac6d"
+      sha256 "0488e0015c8202258412032d1328ad1b35ebd19bd2d7c34c92d047f47538ab4a"
     end
   end
 
