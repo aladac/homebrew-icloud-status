664a461387fb51ae23fc292b9bffa2701cfc9dac
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-05 17:26:53 +0000

    Update hu to 0.1.14

--- Formula/hu.rb
+++ Formula/hu.rb
@@ -1,25 +1,25 @@
 class Hu < Formula
   desc "Token-efficient dev workflow CLI for Claude Code"
   homepage "https://github.com/saiden-dev/hu"
-  version "0.1.13"
+  version "0.1.14"
   license "BUSL-1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/saiden-dev/hu/releases/download/v#{version}/hu-macos-arm64"
-      sha256 "bbd977e3bb90a128806318d2ef8a41e6aa2aa8f50ae0dff6906219cac2a6de18"
+      sha256 "786c6a2f3431d0d52fd4572c1791821434d69cbeb8198d99021c9f3935ab59fb"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/saiden-dev/hu/releases/download/v#{version}/hu-linux-arm64"
-      sha256 "1e6ef1af74f197d6f14128533d829e1e84a2ac5ecb2ae3867d84ea1d74c2cec6"
+      sha256 "6fb7592f491c22d75581aa2cee05a220c23fefd32c6ffdc508c511bc27eb876e"
     end
 
     on_intel do
       url "https://github.com/saiden-dev/hu/releases/download/v#{version}/hu-linux-amd64"
-      sha256 "56c30353346ef0bba69f16493cef1bc88171ec1b843b8f912d72e808f6b7e796"
+      sha256 "afbab665eb05217f72dcd16e109974b63b6b258cee21a4e6f2f1411651ee409e"
     end
   end
 
