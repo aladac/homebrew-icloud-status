315ac94b313279b8436f6e835ca40a2c7327390e
Adam Ladachowski <adam.ladachowski@gmail.com>
2026-04-13 11:28:22 +0000

    Update browse to 0.3.3

--- Formula/browse.rb
+++ Formula/browse.rb
@@ -1,8 +1,8 @@
 class Browse < Formula
   desc "Headless browser automation for Claude Code using Playwright WebKit"
   homepage "https://github.com/saiden-dev/browse"
-  url "https://registry.npmjs.org/@saiden/browse/-/browse-0.3.2.tgz"
-  sha256 "db6dbe10160858d7a2bf8dcc74f8f42ef19810f36b1634a8f908e4605b6674b6"
+  url "https://registry.npmjs.org/@saiden/browse/-/browse-0.3.3.tgz"
+  sha256 "94ad00a7148604cd043b5a41663dbbdca11a9c1e8b1995085e4fc48e8e4f1d34"
   license "MIT"
 
   depends_on "node"
